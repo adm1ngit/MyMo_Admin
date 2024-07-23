e0b0ba98af398524306634066bba2ff484062671
adm1n_217 <142495809+adm1ngit@users.noreply.github.com>
2024-07-23 15:56:53 +0500

    Deploying Fixed

--- conf/settings.py
+++ conf/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["my-mo-admin.vercel.app"]
+ALLOWED_HOSTS = ["my-mo-admin.vercel.app", "127.0.0.1"]
 
 load_dotenv()
 
