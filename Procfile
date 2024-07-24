9fc6fc83d223c26ddcf211b40abe28673a0f9856
adm1n_217 <142495809+adm1ngit@users.noreply.github.com>
2024-07-24 11:25:43 +0500

    Fixed

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn conf.wsgi --log-file -
+web: gunicorn conf.wsgi
--- conf/settings.py
+++ conf/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["192.168.1.22"]
+ALLOWED_HOSTS = ["*"]
 
 load_dotenv()
 
@@ -70,7 +70,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://192.168.1.19",
     "http://127.0.0.1:8000",
-    "https://192.168.1.22",
+    "http://192.168.1.22",
 
 ]
 
